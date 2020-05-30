5bbdb5cf7242e86d5287fd2c2762095eee6ce5e5
Shivam Satyarthi <shivamsatyarthi@Shivams-MBP.fios-router.home>
2020-05-30 16:17:51 -0400

    monitor adjustments

--- src/containers/Monitor/index.js
+++ src/containers/Monitor/index.js
@@ -27,9 +27,9 @@ class Monitor extends React.Component {
             const heartClass = this.beat ? 'monitor__heart-icon-start' : 'monitor__heart-icon-end';
             random = Math.floor(Math.random() * 500) + 2000;
             this.beat = !this.beat;
-            
+
             const coin = Math.floor(Math.random() * 15);
-            let o2Levels = Math.floor(((this.state.o2Levels * coin) + ((Math.random() * 30) + 70))/(coin + 1));
+            let o2Levels = Math.floor(((this.state.o2Levels * coin) + ((Math.random() * 30) + 70)) / (coin + 1));
             this.setState({
                 bpm: Math.floor(Math.random() * 5) + 70,
                 o2Levels,
@@ -38,8 +38,8 @@ class Monitor extends React.Component {
         }, random)
     }
 
-  
-    
+
+
 
     componentWillUnmount() {
         clearInterval(this.refreshInterval);
@@ -52,69 +52,78 @@ class Monitor extends React.Component {
         const { classes, theme } = this.props;
         return (
             <React.Fragment>
-                
-            <div className="monitor">
 
-                
+                <div className="monitor">
+
+                    <div className="monitor__card">
+                        <h4 className="monitor__title">Coughing Audio</h4>
+                        <div className="monitor__audio">
+                            
+                        </div>
+                    </div>
 
 
-                <div className="monitor__card">
-                    <h4 className={"monitor__title"}>Blood sp O2</h4>
-                    <div  className="monitor__speedometer">
-                        <h4>{o2Levels}</h4>
-                    <ReactSpeedometer
-                       
-                        height={185}
-                        width={300}
-                        value={o2Levels}
-                        minValue={70}
-                        maxValue={100}
-                        needleTransitionDuration={1000}
-        needleTransition="easeElastic"
-        customSegmentStops={[70, 85,92, 95, 100]}
-        segmentColors={["#d4daee", "#859bdd", "#2c54c9","#0031bf"]}
-                        customSegmentLabels={[
-                            {
-                              text: "Danger",
-                              position: "OUTSIDE",
-                              color: "rgb(50,50,50)",
-                            },
-                 
-                            {
-                              text: "Low",
-                              position: "OUTSIDE",
-                              color: "rgb(50,50,50)",
-                            },
-                            {
-    
-                                position: "OUTSIDE",
-                                color: "rgb(50,50,50)",
-                              },
-                              {
-                                text: "Healthy",
-                                position: "OUTSIDE",
-                                color: "rgb(50,50,50)",
-                              },
-                          ]}
-                        needleColor="#D8DEE9"
-                        textColor={'#fff'}
-                    />
+                    <div className="monitor__card">
+                        <h4 className={"monitor__title"}>Blood sp O2</h4>
+                        <div className="monitor__speedometer">
+                            <ReactSpeedometer
+
+                                height={120}
+                                width={250}
+                                value={o2Levels}
+                                minValue={70}
+                                maxValue={100}
+                                needleTransitionDuration={1000}
+                                needleTransition="easeElastic"
+                                customSegmentStops={[70, 85, 92, 95, 100]}
+                                segmentColors={["#d4daee", "#859bdd", "#2c54c9", "#0031bf"]}
+                                customSegmentLabels={[
+                                    {
+                                        text: "Danger",
+                                        position: "OUTSIDE",
+                                        color: "rgb(50,50,50)",
+                                    },
+
+                                    {
+                                        text: "Low",
+                                        position: "OUTSIDE",
+                                        color: "rgb(50,50,50)",
+                                    },
+                                    {
+
+                                        position: "OUTSIDE",
+                                        color: "rgb(50,50,50)",
+                                    },
+                                    {
+                                        text: "Healthy",
+                                        position: "OUTSIDE",
+                                        color: "rgb(50,50,50)",
+                                    },
+                                ]}
+                                needleColor="#D8DEE9"
+                                textColor={'#fff'}
+                            />
+                        </div>
+                        <div className="monitor__row">
+                        <div className="monitor__o2-levels">{o2Levels}%</div>
+                        </div>
+                    </div>
+                    <div className="monitor__card">
+                        <span className={"monitor__title"}>Heart Rate</span>
+
+                        <Icon path={mdiHeartPulse}
+                            title="User Profile"
+                            size={'18vh'}
+
+
+                            className={heartClass}
+                        />
+                        <div className="monitor__row">
+                        <div className="monitor__bpm-rate">{bpm} bpm</div>
+                        </div>
+                    </div>
+
                 </div>
-                </div>
-                <div className="monitor__card">
-                    <span className={"monitor__title"}>Heart Rate</span>
-            
-                    <Icon path={mdiHeartPulse}
-                        title="User Profile"
-                        size={15}
-        
-                
-                        className={heartClass}
-                    />
-                    <span className="monitor__bpm-rate">{bpm} bpm</span>
-                </div>
-
-            </div>
             </React.Fragment>
         )
     }
